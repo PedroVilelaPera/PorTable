3bc3c0bcc72e4a21e90808d94ae536a965aee6f7
Pedro Vilela Pereira <152188605+PedroVilelaPera@users.noreply.github.com>
2025-12-08 11:31:43 -0300

    feat: Logout Working!

--- public/js/controller/auth_controller.js
+++ public/js/controller/auth_controller.js
@@ -6,37 +6,46 @@ document.addEventListener('DOMContentLoaded', () => {
     const emailInput = document.getElementById('email');
     const passwordInput = document.getElementById('password');
     const loginButton = document.getElementById('loginBtn');
-    //const errorMessage = document.getElementById('error-message');  
 
     // logout button
     const logoutButton = document.getElementById('logoutBtn');
-      
 
     initAuth((user) => {
-        // login
+
+        const currentPath = window.location.pathname;
+
         if (user) {
-            console.log('User is already logged in:', user);
-            window.location.href = '/workshop.html';
-        }
-        loginForm.addEventListener('submit', async (e) => {
-            e.preventDefault();
-            const email = emailInput.value;
-            const password = passwordInput.value;
-
-            loginButton.disabled = true;
-            loginButton.textContent = 'Logging in...';
-
-            try {
-                await login(email, password);
-                console.log('Login successful');
+            console.log('User is logged in:', user);
+            // Verify current path
+            if (!currentPath.includes('workshop.html')) {
                 window.location.href = '/workshop.html';
-            } catch (error) {
-                console.error('Login failed:', error);
-                alert('Login failed: ' + error.message);
-                loginButton.disabled = false;
-                loginButton.textContent = 'Login';
             }
-        });
+        } else {
+            // Not logged in
+            if (currentPath.includes('workshop.html')) {
+                window.location.href = '/index.html';
+            }
+        }
+        if (loginForm) {
+            loginForm.addEventListener('submit', async (e) => {
+                e.preventDefault();
+                const email = emailInput.value;
+                const password = passwordInput.value;
+
+                loginButton.disabled = true;
+                loginButton.textContent = 'Logging in...';
+
+                try {
+                    await login(email, password);
+                    console.log('Login successful');
+                } catch (error) {
+                    console.error('Login failed:', error);
+                    alert('Login failed: ' + error.message);
+                    loginButton.disabled = false;
+                    loginButton.textContent = 'Login';
+                }
+            });
+        }
 
         // logout
         if (logoutButton) {
